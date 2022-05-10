9ee083ede2b40b458db60761a8c5bd4177f10806
AntonKornev <anton.kornev@sendpulse.com>
2022-05-10 18:10:32 +0300

    Fixed bugs

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import HeaderProfileView from './view/header-profile-view';
 import MainNavigationView from './view/main-navigation-view';
+import SortView from './view/sort-view';
 import FooterStatisticsView from './view/footer-statistics-view';
 import FilmsBoardPresenter from './presenter/films-board-presenter';
 
@@ -11,7 +12,8 @@ const siteFooterElement = document.querySelector('.footer');
 const filmsBoardPresenter = new FilmsBoardPresenter();
 
 render(new HeaderProfileView(), siteHeaderElement);
-render(new MainNavigationView(), siteHeaderElement, RenderPosition.AFTERBEGIN);
+render(new MainNavigationView(), siteMainElement, RenderPosition.AFTERBEGIN);
+render(new SortView(), siteMainElement);
 render(new FooterStatisticsView(), siteFooterElement);
 
 filmsBoardPresenter.init(siteMainElement);
--- src/presenter/films-board-presenter.js
+++ src/presenter/films-board-presenter.js
@@ -3,9 +3,8 @@ import FilmCardView from '../view/film-card-view';
 import FilmsListView from '../view/films-list-view';
 import FilmsListExtraView from '../view/films-list-extra-view';
 import FilmsPopupView from '../view/films-popup-view';
-import SortView from '../view/sort-view';
 import ShowMoreButtonView from '../view/show-more-button-view';
-import {render} from "../render";
+import {render} from '../render';
 
 export default class FilmsBoardPresenter {
   filmsBoardComponent = new FilmsBoardView();
@@ -15,13 +14,14 @@ export default class FilmsBoardPresenter {
     this.filmsBoardContainer = filmsBoardContainer;
 
     render(this.filmsBoardComponent, this.filmsBoardContainer);
-    render(new SortView(), this.filmsBoardComponent.getElement());
     render(this.filmsListComponent, this.filmsBoardComponent.getElement());
 
-    for (let i = 0; i < 3; i++) {
-      render(new FilmCardView(), this.filmsListComponent.getElement());
+    const filmCardContainer = document.querySelector('.films-list__container');
+
+    for (let i = 0; i < 5; i++) {
+      render(new FilmCardView(), filmCardContainer);
     }
 
-    render(new ShowMoreButtonView(), this.filmsBoardComponent.getElement());
-  }
+    render(new ShowMoreButtonView(), this.filmsListComponent.getElement());
+  };
 }
